348595fbf94abbf93cfcc0d00ca8cb089db2a35c
julian94 <langlo94@gmail.com>
2025-11-23 21:54:22 +0100

    Prepped AoC solution.

---
+++ Advent/Program.cs
@@ -0,0 +1,22 @@
+﻿using Advent2025;
+
+Console.WriteLine("Hello, Advent of Code!");
+
+var day = int.Parse(args[0]);
+
+var partOne = day switch
+{
+    1 => Day01.PartOne(File.ReadAllText("Day01.txt")),
+    _ => throw new NotImplementedException(),
+};
+
+Console.WriteLine(partOne);
+
+
+var partTwo = day switch
+{
+    1 => Day01.PartTwo(File.ReadAllText("Day01.txt")),
+    _ => throw new NotImplementedException(),
+};
+
+Console.WriteLine(partTwo);
---
+++ Test/TestDay01.cs
@@ -0,0 +1,29 @@
+﻿using Advent2025;
+using NUnit.Framework;
+namespace TestAdvent2025;
+
+public class TestDay01Part1
+{
+    [Test]
+    public static void MatchesExample()
+    {
+        var exampleInput = """
+
+        """;
+        var exampleResult = "";
+        Assert.That(Day01.PartOne(exampleInput), Is.EqualTo(exampleResult));
+    }
+}
+
+public class TestDay01Part2
+{
+    [Test]
+    public static void MatchesExample()
+    {
+        var exampleInput = """
+
+        """;
+        var exampleResult = "";
+        Assert.That(Day01.PartTwo(exampleInput), Is.EqualTo(exampleResult));
+    }
+}
